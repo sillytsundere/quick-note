3b969cb3eb2e27401a18fc6e67e7bd257f948d60
Paige <paigehcarroll@gmail.com>
2023-08-27 22:35:22 -0500

    making pseudo change to test gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 //init modules (express)
-const express = require('express');
+const express = require("express");
 
 //import module routes for notes
-const notesRoute = require('./notes');
+const notesRoute = require("./notes");
 const app = express();
 
 //middleware to tie route to notes pathway
-app.use('/notes', notesRoute);
+app.use("/notes", notesRoute);
 
 //middleware error handler for server errors
-app.use((err, req, res, next) => 
-    res.status(500).send("Internal Server Error")
-);
+app.use((err, req, res, next) => res.status(500).send("Internal Server Error"));
+
+//making pseudo change to test gitignore
 
 //export app module
 module.exports = app;
